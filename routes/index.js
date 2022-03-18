bc868e653d4d5456bf0eeb4bda29fd751b23ed48
emil aberg <emil.aberg@elev.ga.ntig.se>
2022-03-18 09:31:55 +0100

    TEMPLATE

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
+/* GET home page. */
+router.get('/views/', async  function(req, res, next) {
+  let  data = {
+    message: 'Tjenare världen!!!!!',
+    layout:  'layout.njk',
+    title: 'Yo mcflow'
+  }
+
+  res.render('index.njk', data)
+})
 module.exports = router;
---
+++ routes/meeps.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const pool = require('../database');
+const router = express.Router();
+
+/*
+    BASE URL /meeps
+    GET / - Get all meeps
+    POST / - Create a new task
+    GET /:id - Get a task by id
+    PUT/:id - Update a task by id
+    DELETE /:id - Delete a task by id
+    Create, Read, Update, Delete - CRUD
+
+    read - select
+    create - insert
+    
+*/
+
+/* GET home page. */
+router.get('/', async (req, res, next) => {
+    await pool.promise()
+        .query('SELECT * FROM meeps')
+        .then(([rows, fields]) => {
+            console.log(rows);
+            res.json({
+                meeps: {
+                    data: rows
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                meeps: {
+                    error: 'Error gettings meeps'
+                }
+            })
+        })
+});
+
+
+router.get('/:id', async (req, res, next) => {
+
+    const id = req.params.id;
+    if (isNaN(req.params.id)) {
+        res.status(400).json({
+            task: {
+                error: 'Bad request'
+            }
+        });
+
+    }
+    res.json({
+        id: req.params.id
+    })
+});
+
+
+module.exports = router;
