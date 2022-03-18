4141e319c207352b474fd8db95ed09ec8a333392
emil aberg <emil.aberg@elev.ga.ntig.se>
2022-03-18 11:22:32 +0100

    cards, tweets, add tweet

--- routes/meeps.js
+++ routes/meeps.js
@@ -5,10 +5,10 @@ const router = express.Router();
 /*
     BASE URL /meeps
     GET / - Get all meeps
-    POST / - Create a new task
-    GET /:id - Get a task by id
-    PUT/:id - Update a task by id
-    DELETE /:id - Delete a task by id
+    POST / - Create a new tweet
+    GET /:id - Get a tweet by id
+    PUT/:id - Update a tweet by id
+    DELETE /:id - Delete a tweet by id
     Create, Read, Update, Delete - CRUD
 
     read - select
@@ -43,7 +43,7 @@ router.get('/:id', async (req, res, next) => {
     const id = req.params.id;
     if (isNaN(req.params.id)) {
         res.status(400).json({
-            task: {
+            tweet: {
                 error: 'Bad request'
             }
         });
@@ -52,6 +52,35 @@ router.get('/:id', async (req, res, next) => {
     res.json({
         id: req.params.id
     })
+});
+router.post('/', async (req, res, next) => {
+    // { "tweet": "koda post" }
+    const tweets = req.body.tweets;
+
+    await pool
+        .promise()
+        .query('INSERT INTO meeps (body) VALUES (?)', [tweets])
+        .then((response) => {
+            if (response[0].affectedRows === 1) {
+                res.redirect('/meeps');
+            } else {
+                res.status(400).json({
+                    tweet: {
+                        error: 'Invalid tweet',
+                    },
+                });
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+                tweet: {
+                    error: 'Error getting tweets',
+                },
+            });
+        });
+
+    // res.json(req.body);
 });
 
 
